d13bd4fd42893d1eb81924e1e155d486b41c6e6d
subhajit dhar <subhajit13dhar@gmail.com>
2024-11-26 19:55:54 +0530

    Added The beauty in navbar

---
+++ app/(forms)/AddList/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const AddList = () => {
+  return (
+    <div>AddList</div>
+  )
+}
+
+export default AddList;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
+import React, {useState} from 'react';
+import { IoMdAddCircle } from "react-icons/io";
+import styles from "./styles.module.css";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    const pathname = usePathname();
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <nav className='flex items-center justify-center gap-7 h-5 p-7 mb-5 mt-5'>
+            <div className='px-4 py-4 '>
+                <h1 className='text-3xl font-serif'>
+                    <Link href="/">TODO APP</Link>
+                </h1>
+            </div>
+            <div className={`relative px-4 py-4 ${pathname === '/AddList' ? ' hidden ' : ''}`} 
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            >
+                {/* Add Button */}
+                <Link href="/AddList">
+                    <IoMdAddCircle className={`${styles.icon} text-4xl text-white`} />
+                </Link>
 
-export default Navbar
+                {/* Hover Text */}
+                {isHovered && (
+                    <span
+                        className="absolute px-2 py-1  text-white text-nowrap text-xs font-light rounded shadow-lg"
+                    >
+                        Add Items
+                    </span>
+                )}
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
